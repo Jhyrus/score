ee0416b9da7ffd2d6629d53b89c4257c0dfb934a
Victor Aguilar Cusicanqui <jvacx.log@gmail.com>
2013-10-08 11:03:48 -0400

    contexto extra en una vista

--- finish/wall/views.py
+++ finish/wall/views.py
@@ -1,12 +1,23 @@
 # -*- encoding: utf-8 -*-
 from django.views.generic import ListView
 
-from .models import Post
+from .models import Post, Announcement, ImageGalery
 
 class PostList(ListView):
 	template_name = "home.html"
 	model = Post
 	context_object_name = 'posts'
+
+	def get_context_data(self, **kwargs):
+		context = super(PostList, self).get_context_data(**kwargs)
+
+		#son los anuncios y las imagenes de las galerias que estan a los costados deberias cargar solo los
+		# que entren en la pantalla para eso modifica el la consulta
+		context['announcements'] = Announcement.objects.all().order_by("-id")
+		context['images'] = ImageGalery.objects.all.order_by("-id")
+
+		return context
+
 
 # Create your views here.
 
@@ -16,6 +27,8 @@ class ContactView(ListView):
 	model = Post
 	context_object_name = 'posts'
 	paginate_by = 6
+
+
 
 
 class InternacionalView(ListView):
